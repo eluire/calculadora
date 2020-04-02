72881c2a586514c3312d7baa28c3601b74d234a9
eluire <eluire@gmail.com>
2020-04-02 18:05:46 -0300

    Updates

---
+++ precache-manifest.8faa59b15310b1f5ecf9f415fed55ae7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5962a7db69dd4b7a27a8a3e3ad1dfe2e",
+    "url": "/calculadora/index.html"
+  },
+  {
+    "revision": "27be9bfe778fa332d992",
+    "url": "/calculadora/static/css/main.25a2a15d.chunk.css"
+  },
+  {
+    "revision": "6354c95a4652f354b1c0",
+    "url": "/calculadora/static/js/2.b3693d92.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/calculadora/static/js/2.b3693d92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27be9bfe778fa332d992",
+    "url": "/calculadora/static/js/main.547ec836.chunk.js"
+  },
+  {
+    "revision": "4fb3a74b099f258b5f36",
+    "url": "/calculadora/static/js/runtime-main.6338f19d.js"
+  }
+]);
