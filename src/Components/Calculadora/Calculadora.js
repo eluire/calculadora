bf133a008dfa75446ff8b938c012d5d278e6a090
eluire <eluire@gmail.com>
2020-04-03 11:50:58 -0300

    concertando bug segundo valor digitado

--- src/Components/Calculadora/Calculadora.js
+++ src/Components/Calculadora/Calculadora.js
@@ -3,117 +3,103 @@ import Visor from '../Visor/Visor'
 import Botoes from '../Botoes/Botoes'
 import './Calculadora.css'
 
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0
+}
+
 class Calculadora extends Component {
-    state = {
-        valor_display: '0',
-        clear_display: false,
-        operation: null,
-        valores: [0, 0],
-        index: 0
-    }
-    limpar_memoria(){
-        this.setState({
-            valor_display: '0',
-            clear_display: false,
-            operation: null,
-            valores: [0, 0],
-            index: 0
-        })
-    }
-    add_operacao(operacao){
-        if ( this.state.index === 0) {
-            this.setState({operation: operacao, index:1, clear_display:true})
-        }
-        else{
-            const equals = operacao === '='
-            const current_op = this.state.operation
 
-            const valores = [...this.state.valores]
+    state = { ...initialState }
 
-            switch (current_op) {
-                case '+':
-                    valores[0] = valores[0] + valores[1]
-                    break
-                case '-':
-                    valores[0] = valores[0] - valores[1]
-                    break
-                case '*':
-                    valores[0] = valores[0] * valores[1]
-                    break
-                case '/':
-                    try {
-                        valores[0] = valores[0] / valores[1]
-                    }catch(e){
-                        valores[0] = this.state.valores[0]
-                        console.log('erro!! divisão por 0')    
-                    }
-                    break
-                default:
-                    break
+    constructor(props) {
+        super(props)
+
+        this.limpar_memoria = this.limpar_memoria.bind(this)
+        this.add_operacao = this.add_operacao.bind(this)
+        this.add_digito = this.add_digito.bind(this)
+    }
+
+    limpar_memoria() {
+        this.setState({ ...initialState })
+    }
+
+    add_operacao(operation) {
+        if (this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true })
+        } else {
+            const equals = operation === '='
+            const currentOperation = this.state.operation
+
+            const values = [...this.state.values]
+            try {
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+            } catch(e) {
+                values[0] = this.state.values[0]
             }
-            valores[1] = 0
+
+            values[1] = 0
 
             this.setState({
-                valor_display: valores[0],
-                operation: equals ? null : operacao,
-                index: equals ? 0 : 1,
-                clear_display: !equals,
-                valores
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
             })
         }
     }
 
-    add_digito(n){
-        if ( n === '.' && this.state.valor_display.includes('.')){
+    add_digito(n) {
+        if (n === '.' && this.state.displayValue.includes('.')) {
             return
         }
-        const clear_display = this.state.valor_display === '0' || this.state.clear_display
 
-        const current_value = clear_display ? '' : this.state.valor_display
-        const display_value = current_value + n
-
-        this.setState({valor_display: display_value, limpar_memoria: false})
+        const clearDisplay = this.state.displayValue === '0'
+            || this.state.clearDisplay
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+        const displayValue = currentValue + n
+        this.setState({ displayValue, clearDisplay: false })
 
         if (n !== '.') {
-            const i = this.state.index
-            const novo_valor = parseFloat(display_value)
-            const valores = [...this.state.valores]
-            valores[i] = novo_valor
-            this.setState({valores})
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[i] = newValue
+            this.setState({ values })
+            console.log(values)
         }
     }
+
     render(){
-        
-       
-        const add_dig = (n) => this.add_digito(n)
-        const add_op = (op) => this.add_operacao(op)
-        
         return(
             <>
                 <h1>CALCULADORA</h1>
                 <div className= 'calculator'>
-                    <Visor value= { this.state.valor_display }/>
-                    <Botoes label= 'AC' click ={ () => this.limpar_memoria() } triple/>
-                    <Botoes label= '/' click = {add_op} operation/>
-                    <Botoes label= '7' click = {add_dig}/>
-                    <Botoes label= '8' click = {add_dig}/>
-                    <Botoes label= '9' click = {add_dig}/>
-                    <Botoes label= '*' click = {add_op} operation/>
-                    <Botoes label= '4' click = {add_dig}/>
-                    <Botoes label= '5' click = {add_dig}/>
-                    <Botoes label= '6' click = {add_dig}/>
-                    <Botoes label= '-' click = {add_op} operation/>
-                    <Botoes label= '1' click = {add_dig}/>
-                    <Botoes label= '2' click = {add_dig}/>
-                    <Botoes label= '3' click = {add_dig}/>
-                    <Botoes label= '+' click = {add_op} operation/>
-                    <Botoes label= '0' click = {add_dig} double/>
-                    <Botoes label= '.' click = {add_dig}/>
-                    <Botoes label= '=' click = {add_op} operation/>
+                    <Visor value= { this.state.displayValue }/>
+                    <Botoes label= 'AC' click ={this.limpar_memoria} triple/>
+                    <Botoes label= '/' click = {this.add_digito} operation/>
+                    <Botoes label= '7' click = {this.add_digito}/>
+                    <Botoes label= '8' click = {this.add_digito}/>
+                    <Botoes label= '9' click = {this.add_digito}/>
+                    <Botoes label= '*' click = {this.add_operacao} operation/>
+                    <Botoes label= '4' click = {this.add_digito}/>
+                    <Botoes label= '5' click = {this.add_digito}/>
+                    <Botoes label= '6' click = {this.add_digito}/>
+                    <Botoes label= '-' click = {this.add_operacao} operation/>
+                    <Botoes label= '1' click = {this.add_digito}/>
+                    <Botoes label= '2' click = {this.add_digito}/>
+                    <Botoes label= '3' click = {this.add_digito}/>
+                    <Botoes label= '+' click = {this.add_operacao} operation/>
+                    <Botoes label= '0' click = {this.add_digito} double/>
+                    <Botoes label= '.' click = {this.add_digito}/>
+                    <Botoes label= '=' click = {this.add_operacao} operation/>
                 </div>
             </>
         )
     }
 }
-
 export default Calculadora
